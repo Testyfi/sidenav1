8e66080bc9ab73c3b513406eb2e91c6de2004bc4
Anuj Kumar Sharma <anujmyn@gmail.com>
2023-08-15 09:15:47 +0530

    loggin update

--- src/app/coupens/coupens.component.ts
+++ src/app/coupens/coupens.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { ProfilepictureupdateService } from '../profilepictureupdate.service';
 @Component({
   selector: 'app-coupens',
   templateUrl: './coupens.component.html',
-  styleUrls: ['./coupens.component.scss']
+  styleUrls: ['./coupens.component.scss'],
 })
 export class CoupensComponent {
-
+  constructor(public profile: ProfilepictureupdateService) {}
 }
