1329275587878af4733b3f52dbc2b76343cd1302
Anuj Kumar Sharma <anujmyn@gmail.com>
2023-10-31 14:46:11 +0530

    updatepayment

--- src/app/coupens/coupens.component.ts
+++ src/app/coupens/coupens.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ProfilepictureupdateService } from '../profilepictureupdate.service';
 import { PaymentService } from '../payment.service';
 import { Paymentresponse } from '../paymentresponse';
+import { Data } from '../paymentresponse';
 @Component({
   selector: 'app-coupens',
   templateUrl: './coupens.component.html',
@@ -14,11 +15,18 @@ export class CoupensComponent {
   ) {}
 
   makepayrequest(amount: any) {
+    var d: Data = { payment_url: '' };
+    var pay: Paymentresponse = {
+      success: false,
+      code: 409,
+      message: 'nomessage',
+      data: d,
+    };
     var token: any = this.profile.getprofile()().token;
-    var pay;
+    var pay: Paymentresponse;
     this.payment
       .makePaymentRequest(amount, token)
       .subscribe((data) => (pay = data));
-    console.log(pay);
+    console.log(pay.data.payment_url);
   }
 }
