b06ea15c0c87735a4130b09286d03e49393b8274
Anuj Kumar Sharma <anujmyn@gmail.com>
2023-11-01 19:15:57 +0530

    updatepayment

--- src/app/coupens/coupens.component.ts
+++ src/app/coupens/coupens.component.ts
@@ -57,8 +57,8 @@ export class CoupensComponent {
       //value=data.data.payment_url;
       //this.seturl(data);
       //console.log(this.pay.data.payment_url);
-      //window.open(this.pay.data.payment_url);
-      window.location.href = data.data.payment_url;
+      window.open(this.pay.data.payment_url, '_blank');
+      // window.location.href = data.data.payment_url;
       //window.location.assign(this.pay.data.payment_url);
       //window.location.href = this.pay.data.payment_url;
       //this.router.navigate([this.pay.data.payment_url]);
