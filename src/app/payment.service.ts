38f7f0d930ad91f871d4558d96025d0903b005cd
Anuj Kumar Sharma <anujmyn@gmail.com>
2023-11-28 09:47:21 +0530

    pasttest

--- src/app/payment.service.ts
+++ src/app/payment.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Paymentresponse } from './paymentresponse';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class PaymentService {
   constructor(private http: HttpClient) {}
-  private apiUrl = 'http://localhost:8080/payment/phonepay/request';
+  //private apiUrl = 'http://localhost:8080/payment/phonepay/request';
   makePaymentRequest(
     amount: number,
     token: string
@@ -19,9 +19,13 @@ export class PaymentService {
       amount: amount,
     };
 
-    return this.http.post<Paymentresponse>(this.apiUrl, body, {
-      headers: this.getHeader(),
-    });
+    return this.http.post<Paymentresponse>(
+      `${environment.backend}/payment/phonepay/request`,
+      body,
+      {
+        headers: this.getHeader(),
+      }
+    );
   }
   getHeader() {
     let str: any = '';
